3418f9a043001671b23d31f4df153e176c3e6c3f
0phoff <0phoff@users.noreply.github.com>
2018-06-01 09:56:00 +0200

    Lowercase folders pls

--- install.sh
+++ install.sh
@@ -22,12 +22,12 @@ HOME=$HOME
 SCRIPT="$HOME/bin"
 
 # Bash folder
-$DEBUG ln -sf $PWD/Bash/agignore            $HOME/.agignore
-$DEBUG ln -sf $PWD/Bash/bashrc              $HOME/.bashrc
-$DEBUG ln -sf $PWD/Bash/global.gitconfig    $HOME/.gitconfig
-$DEBUG ln -sf $PWD/Bash/global.gitignore    $HOME/.gitignore
+$DEBUG ln -sf $PWD/bash/agignore            $HOME/.agignore
+$DEBUG ln -sf $PWD/bash/bashrc              $HOME/.bashrc
+$DEBUG ln -sf $PWD/bash/global.gitconfig    $HOME/.gitconfig
+$DEBUG ln -sf $PWD/bash/global.gitignore    $HOME/.gitignore
 
-for f in $PWD/Bash/*.sh
+for f in $PWD/bash/*.sh
 do
     base=$(basename $f)
     case "$base" in
@@ -44,15 +44,15 @@ do
 done
 
 # Tmux folder
-$DEBUG ln -sf $PWD/Tmux/tmux.conf            $HOME/.tmux.conf
+$DEBUG ln -sf $PWD/tmux/tmux.conf            $HOME/.tmux.conf
 
 # Vim folder
 mkdir -p $HOME/.config/nvim
-$DEBUG ln -sf $PWD/Vim/neovim.vim            $HOME/.config/nvim/init.vim
-$DEBUG ln -sf $PWD/Vim/ftdetect              $HOME/.config/nvim/ftdetect
-$DEBUG ln -sf $PWD/Vim/ftplugin              $HOME/.config/nvim/ftplugin
-$DEBUG ln -sf $PWD/Vim/scripts               $HOME/.config/nvim/scripts
-$DEBUG ln -sf $PWD/Vim/syntax                $HOME/.config/nvim/syntax
+$DEBUG ln -sf $PWD/vim/neovim.vim            $HOME/.config/nvim/init.vim
+$DEBUG ln -sf $PWD/vim/ftdetect              $HOME/.config/nvim/ftdetect
+$DEBUG ln -sf $PWD/vim/ftplugin              $HOME/.config/nvim/ftplugin
+$DEBUG ln -sf $PWD/vim/scripts               $HOME/.config/nvim/scripts
+$DEBUG ln -sf $PWD/vim/syntax                $HOME/.config/nvim/syntax
 
 # Print todo
 if [ -z ${var+x} ]; then
