697529cbde09ac9cd8c1c4d53c4551438e107048
0phoff <0phoff@users.noreply.github.com>
2018-07-13 19:38:14 +0200

    Changed metadata stuff

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# 0phoff's Personal DotFiles
+![Logo](https://raw.githubusercontent.com/0phoff/dotfiles/master/logo.png)  
+
+# 0phoff's DOT-files
 This repo contains my personal dotfiles.  
 Feel free to look around, copy and redistribute at your own will!  
 I would not recomend blindly taking over my dotfiles, as they contain quite a lot of aliasses, vim mappings, etc. It is always better to build your own dotfiles!
-
-## Different Files Explained
-Like a lot of people, I keep all my dotfiles in a separate folder and create symlinks at the places where they should be.  
-
-  - Bash:     In this directory you can find my bashrc as well as other scripts I link to /usr/local/bin
-  - Tmux:     This folder contains my tmux configuration
-  - Vim:      This folder contains my Vim configuration. I use neovim, so that's a thing to consider...
-  - Keyboard: This folder contains some xkb settings for my keyboard
--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #----------------------------------------------------------------------------------------------------------------------
 # DOTFILES INSTALL SCRIPT
 #----------------------------------------------------------------------------------------------------------------------
 #
 # Dotfiles Install Script : This script will create the appropriate simlinks to install all of the configuration files
 # By 0phoff
-# MIT license
+# GPL license
 #
 #----------------------------------------------------------------------------------------------------------------------
 #
 # Usage: 
-#   ./install.sh
+#   ./install.sh [folder_name]
 #
 # Dry run:
-#   DEBUG=echo ./install.sh
+#   DEBUG=echo ./install.sh [folder_name]
 #
 #----------------------------------------------------------------------------------------------------------------------
 
@@ -21,47 +21,72 @@
 HOME=$HOME
 SCRIPT="$HOME/bin"
 
-# Bash folder
-$DEBUG ln -sf $PWD/bash/agignore            $HOME/.agignore
-$DEBUG ln -sf $PWD/bash/bashrc              $HOME/.bashrc
-$DEBUG ln -sf $PWD/bash/global.gitconfig    $HOME/.gitconfig
-$DEBUG ln -sf $PWD/bash/global.gitignore    $HOME/.gitignore
+# Check argument
+if [ $# -le 0 ]; then
+    echo 'Enter folder to install'
+    exit 1
+fi
 
-for f in $PWD/bash/*.sh
-do
-    base=$(basename $f)
-    case "$base" in
-        tmuxCreator.sh)
-            $DEBUG ln -sf $f $SCRIPT/tmc
-            ;;
-        rosinit.sh)
-            $DEBUG ln -sf $f $SCRIPT/$base
-            ;;
-        *)
-            $DEBUG ln -sf $f $SCRIPT/${base%.*}
-            ;;
-    esac
-done
+if [[ "$1" =~ ^scripts/?$ ]]; then
+    for f in $PWD/scripts/*.sh
+    do
+        base=$(basename $f)
+        case "$base" in
+            tmuxCreator.sh)
+                $DEBUG ln -sf $f $SCRIPT/tmc
+                ;;
+            rosinit.sh)
+                $DEBUG ln -sf $f $SCRIPT/$base
+                ;;
+            *)
+                $DEBUG ln -sf $f $SCRIPT/${base%.*}
+                ;;
+        esac
+    done
+    if [ -z ${DEBUG+x} ]; then
+    	echo 'Installed scripts'
+    fi
+elif [[ "$1" =~ ^rc/?$ ]]; then
+    #$DEBUG ln -sf $PWD/rc/agignore            $HOME/.agignore
+    $DEBUG ln -sf $PWD/rc/bashrc              $HOME/.bashrc
+    $DEBUG ln -sf $PWD/rc/dir_colors	      $HOME/.dir_colors
+    $DEBUG ln -sf $PWD/rc/global.gitconfig    $HOME/.gitconfig
+    $DEBUG ln -sf $PWD/rc/global.gitignore    $HOME/.gitignore
+    $DEBUG ln -sf $PWD/rc/profile	      $HOME/.profile
+    $DEBUG ln -sf $PWD/rc/Xresources	      $HOME/.Xresources
+    if [ -z ${DEBUG+x} ]; then
+        echo 'Installed config files'
+    fi
+elif [[ "$1" =~ ^tmux/?$ ]]; then
+    $DEBUG ln -sf $PWD/tmux/tmux.conf            $HOME/.tmux.conf
+    $DEBUG ln -sf $PWD/tmux/settings.tmux        $HOME/.tmux/settings.tmux
+    $DEBUG ln -sf $PWD/tmux/keymap.tmux          $HOME/.tmux/keymap.tmux
+    $DEBUG ln -sf $PWD/tmux/status-ascii.tmux	 $HOME/.tmux/status-ascii.tmux
+    $DEBUG ln -sf $PWD/tmux/status-power.tmux	 $HOME/.tmux/status-power.tmux
+    if [ -z ${DEBUG+x} ]; then
+        echo 'Installed tmux config'
+    fi
+elif [[ "$1" =~ ^vim/?$ ]]; then
+    mkdir -p $HOME/.config/nvim
+    $DEBUG ln -sf $PWD/vim/neovim.vim            $HOME/.config/nvim/init.vim
+    $DEBUG ln -sf $PWD/vim/ftdetect              $HOME/.config/nvim/ftdetect
+    $DEBUG ln -sf $PWD/vim/ftplugin              $HOME/.config/nvim/ftplugin
+    $DEBUG ln -sf $PWD/vim/scripts               $HOME/.config/nvim/scripts
+    $DEBUG ln -sf $PWD/vim/syntax                $HOME/.config/nvim/syntax
 
-# Tmux folder
-$DEBUG ln -sf $PWD/tmux/tmux.conf            $HOME/.tmux.conf
-
-# Vim folder
-mkdir -p $HOME/.config/nvim
-$DEBUG ln -sf $PWD/vim/neovim.vim            $HOME/.config/nvim/init.vim
-$DEBUG ln -sf $PWD/vim/ftdetect              $HOME/.config/nvim/ftdetect
-$DEBUG ln -sf $PWD/vim/ftplugin              $HOME/.config/nvim/ftplugin
-$DEBUG ln -sf $PWD/vim/scripts               $HOME/.config/nvim/scripts
-$DEBUG ln -sf $PWD/vim/syntax                $HOME/.config/nvim/syntax
-
-$DEBUG rm -f $PWD/vim/ftdetect/ftdetect
-$DEBUG rm -f $PWD/vim/ftplugin/ftplugin
-$DEBUG rm -f $PWD/vim/scripts/scripts
-$DEBUG rm -f $PWD/vim/syntax/syntax
-
-# Print todo
-if [ -z ${var+x} ]; then
-    printf "Check \e[1mINSTALLATION.md\e[0m for further install instructions!\n"
-else
-    printf "\nThese are the \e[1msimlinks\e[0m that will be created if you run this command...\n"
+    # Not sure why these files get created...
+    $DEBUG rm -f $PWD/vim/ftdetect/ftdetect
+    $DEBUG rm -f $PWD/vim/ftplugin/ftplugin
+    $DEBUG rm -f $PWD/vim/scripts/scripts
+    $DEBUG rm -f $PWD/vim/syntax/syntax
+    if [ -z ${DEBUG+x} ]; then
+        echo "Installed neovim setup\nPlease install vim-plug manually and run :PlugInstall in the editor\nhttps://github.com/junegunn/vim-plug"
+    fi
+elif [[ "$1" =~ ^fonts/?$ ]]; then
+    mkdir -p $HOME/.local/share/fonts
+    $DEBUG ln -sf $PWD/fonts /home/top/.local/share/fonts/dotfiles
+    fc-cache -fv
+    if [ -z ${DEBUG+x} ]; then
+        echo 'Installed terminal fonts. Select the right one in the settings of your terminal emulator'
+    fi
 fi
