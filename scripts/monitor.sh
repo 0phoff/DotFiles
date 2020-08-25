878303f0fcb38b05233b8f029b4b118edb2d7273
0phoff <0phoff@users.noreply.github.com>
2020-08-25 13:37:49 +0200

    Monitor script works with nvtop if installed

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -21,9 +21,12 @@ else
     tmc -n Monitor -d $HOME vv
 fi
 
+# Check if nvtop is installed
+command -v nvtop >/dev/null && NVCMD="nvtop" || NVCMD="nvidia-smi"
+
 # Commands
-tmux send -t Monitor.1 'top' Enter
-tmux send -t Monitor.2 'watch -tn0.1 nvidia-smi' Enter
+tmux send -t Monitor.1 "top" Enter
+tmux send -t Monitor.2 "$NVCMD" Enter
 
 # Select main window
 tmux selectw -t Monitor
